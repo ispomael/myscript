53d761a218b97bf80dcc306fe4f922b530d2b4bf
ispomael <ispomael@gmail.com>
2025-07-24 19:34:44 +0200

    Delete get-token.js

--- get-token.js
+++
@@ -1,8 +0,0 @@
-var req = new XMLHttpRequest();
-req.onload = handleResponse;
-req.open('get','/settings/general',true);
-req.send();
-function handleResponse() {
-    var token = this.responseText.match(/name="csrf-token" content="(\w+)"/)[1];
-    console.log(token);
-};
