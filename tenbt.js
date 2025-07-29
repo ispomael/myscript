5648cd60e1f8627d3acafd1c474b45a95548f116
ispomael <ispomael@gmail.com>
2025-07-29 19:18:02 +0200

    Delete tenbt.js

--- tenbt.js
+++
@@ -1,9 +0,0 @@
-var req = new XMLHttpRequest();
-req.onload = handleResponse;
-req.open('get','/document/create',true);
-req.send();
-function handleResponse() {
-    var token = this.responseText.match(/name="csrf-token" content="(\w+)"/)[1];
-    var profileid = this.responseText.match(/class="m-4 text-dark text-center" href="(\w+)"/)[1];
-    console.log(profileid);
-};
